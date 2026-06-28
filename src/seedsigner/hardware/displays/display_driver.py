8117049f300aa75666cdcfb68bbdbaa3f971ea42
Ether Biswas <etherrorcode404@gmail.com>
2026-06-27 20:03:30 -0400

    Tune SeedSigner+ ST7789 display colors
    Apply Waveshare-style color initialization and RGB ordering to the 320x240 ST7789 path for the 2.8 inch IPS 240x320 display profile.

--- src/seedsigner/hardware/displays/display_driver.py
+++ src/seedsigner/hardware/displays/display_driver.py
@@ -35,10 +35,31 @@ class DisplayDriver:
                 self.display = ST7789()
 
             elif width == 320:
-                from seedsigner.hardware.displays.st7789_mpy import ST7789
-                # Have to swap width and height; screen is natively 240x320
-                self.display = ST7789(width=height, height=width)
-        
+                from seedsigner.hardware.displays.st7789_mpy import ST7789, RGB
+                # Have to swap width and height; screen is natively 240x320.
+                # Use the same Waveshare-style color/gamma tuning as the
+                # original 240x240 ST7789 driver while keeping 320x240 geometry.
+                waveshare_color_init = (
+                    (b"\x11", b"\x00", 150),
+                    (b"\x13", b"\x00", 0),
+                    (b"\xb6", b"\x0a\x82", 0),
+                    (b"\x3a", b"\x55", 10),
+                    (b"\xb2", b"\x0c\x0c\x00\x33\x33", 0),
+                    (b"\xb7", b"\x35", 0),
+                    (b"\xbb", b"\x19", 0),
+                    (b"\xc0", b"\x2c", 0),
+                    (b"\xc2", b"\x01", 0),
+                    (b"\xc3", b"\x12", 0),
+                    (b"\xc4", b"\x20", 0),
+                    (b"\xc6", b"\x0f", 0),
+                    (b"\xd0", b"\xa4\xa1", 0),
+                    (b"\xe0", b"\xd0\x04\x0d\x11\x13\x2b\x3f\x54\x4c\x18\x0d\x0b\x1f\x23", 0),
+                    (b"\xe1", b"\xd0\x04\x0c\x11\x13\x2c\x3f\x44\x51\x2f\x1f\x1f\x20\x23", 0),
+                    (b"\x21", b"\x00", 0),
+                    (b"\x29", b"\x00", 120),
+                )
+                self.display = ST7789(width=height, height=width, color_order=RGB, custom_init=waveshare_color_init)
+
         elif self.display_type == DISPLAY_TYPE__ST7735:
             if width != 128 or height != 128:
                 raise ValueError("ST7735 display only supports 128x128 resolution")
@@ -49,7 +70,7 @@ class DisplayDriver:
             from seedsigner.hardware.displays.ili9341 import ILI9341
             self.display = ILI9341()
             self.display.begin()
-        
+
         elif self.display_type == DISPLAY_TYPE__ILI9486:
             # TODO: improve performance of ili9486 driver
             raise Exception("ILI9486 display not implemented yet")
@@ -64,7 +85,7 @@ class DisplayDriver:
 
             # Desktop display can support arbitrary sizes; defaults are handled by caller
             self.display = DesktopDisplay(width=width, height=height)
-    
+
 
     def __str__(self):
         return f"DisplayDriver(display_type={self.display_type}, width={self.width}, height={self.height})"
